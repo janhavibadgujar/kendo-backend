56dd4766c473ea47dd55eecd640aa13bb4307f16
janhavi.badgujar <janhavi.badgujar@thinkitive.com>
2023-04-14 19:28:18 +0530

    Changes in Department API's

--- src/helpers/departmentHelper.js
+++ src/helpers/departmentHelper.js
@@ -3,10 +3,12 @@ const pool = require('../config/dbconfig')
 
 exports.getDepartmentBySiteId = async (siteIds) => {
     const siteIdValues = siteIds.map(site => `CONVERT(uniqueidentifier, '${site}')`).join(',');
-    return request.query(`SELECT ID,SiteID,Name FROM Site WHERE SiteID IN (${siteIdValues}) AND Level = 3`)
+    return await pool.request()
+    .query(`SELECT ID,SiteID,Name FROM Site WHERE SiteID IN (${siteIdValues}) AND Level = 3`)
 }
 
 exports.getDepartmentByCompanyId=async(companyid)=>{
     request.input('param1', sql.VarChar(50), companyid);
-   return request.query('SELECT * FROM Asset WHERE CompanyID = @param1  AND Level = 3')
+    return await pool.request()
+   .query('SELECT * FROM Asset WHERE CompanyID = @param1  AND Level = 3')
 }
